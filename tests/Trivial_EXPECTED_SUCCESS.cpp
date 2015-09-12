3d24da69ed1cd56f89fc136fc02f4aeb20f7760a
Manu343726 <Manu343726@gmail.com>
2015-09-12 21:43:44 +0200

    The guy who break tests by missing semicolons after macros...

--- tests/Trivial_EXPECTED_SUCCESS.cpp
+++ tests/Trivial_EXPECTED_SUCCESS.cpp
@@ -5,9 +5,9 @@
 
 int main()
 {
-	CONCEPT_ASSERT(Trivial,int{})
-	CONCEPT_ASSERT(Trivial,bool{})
-	CONCEPT_ASSERT(Trivial,std::vector<int>{})
+	CONCEPT_ASSERT(Trivial,int{});
+	CONCEPT_ASSERT(Trivial,bool{});
+	CONCEPT_ASSERT(Trivial,std::vector<int>{});
 	//... Here's where I miss reflection, imagine a loop over C++ types
 	//without having to write a typelist by hand
 }
