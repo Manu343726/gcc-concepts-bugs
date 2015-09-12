28ee0166a888a0dfbf296121ab5a9c34446d99d9
Manu343726 <Manu343726@gmail.com>
2015-09-12 21:40:11 +0200

    Fix CONCEPT_ASSERT() macro

--- tests/utils.hpp
+++ tests/utils.hpp
@@ -1,6 +1,6 @@
 #ifndef GCC_CONCEPTS_BUGS_UTILS_HPP
 #define GCC_CONCEPTS_BUGS_UTILS_HPP
 
-#define CONCEPT_ASSERT(concept, value) [](Concept){}(value)
+#define CONCEPT_ASSERT(concept, value) [](concept){}(value)
 
 #endif /* GCC_CONCEPTS_BUGS_UTILS_HPP */
