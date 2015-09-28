46a9d7d71ffbb63fd439c39d2c8aab06d73c56d6
Manu343726 <Manu343726@users.noreply.github.com>
2015-09-28 21:10:38 +0200

    Patch (ñapa) for #1

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p build && cd build
+# Trying to fix bypass issue 1...
+export LD_LIBRARY_PATH="${LD_LIBRAIRY_PATH}:/usr/local/lib/gcc/x86_64-pc-linux-gnu/lib64/"
 cmake .. -DCMAKE_CXX_COMPILER=$(which g++-concepts)
 ctest --verbose
