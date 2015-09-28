bd9b40d338b5f761fadd6d634b2a9d2de9e28fe3
Manu343726 <Manu343726@gmail.com>
2015-09-28 22:01:42 +0200

    Fix Trivial test expected result. Add result to gcb_target() info. Asume EXPECTED_SUCCESS by default

--- src/Trivial.cpp
+++ src/Trivial.cpp
@@ -2,7 +2,7 @@
  * SOURCES: Trivial.cpp
  * INCLUDE_DIRECTORIES: . ..
  * COMPILE_OPTIONS: -std=c++1z -Wall
- * RESULT: EXPECTED_FAIL
+ * RESULT: EXPECTED_SUCCESS
  */
 
 #include "utils.hpp"
@@ -12,9 +12,9 @@
 
 int main()
 {
-	CONCEPT_ASSERT(Trivial,int{});
-	CONCEPT_ASSERT(Trivial,bool{});
-	CONCEPT_ASSERT(Trivial,std::vector<int>{});
-	//... Here's where I miss reflection, imagine a loop over C++ types
-	//without having to write a typelist by hand
+    CONCEPT_ASSERT(Trivial,int{});
+    CONCEPT_ASSERT(Trivial,bool{});
+    CONCEPT_ASSERT(Trivial,std::vector<int>{});
+    //... Here's where I miss reflection, imagine a loop over C++ types
+    //without having to write a typelist by hand
 }
