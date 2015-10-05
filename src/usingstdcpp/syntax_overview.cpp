31351e4ddae6013e921a788f991bf30bae3a1bf2
Manu343726 <Manu343726@gmail.com>
2015-10-05 22:13:11 +0200

    Use semicolon to separate requirements. Only compount requirements need braces

--- src/usingstdcpp/syntax_overview.cpp
+++ src/usingstdcpp/syntax_overview.cpp
@@ -4,30 +4,31 @@
 
 template<typename T>
 concept bool UnaryConcept() {
-	return sizeof(T) > 0;
+	return sizeof(T) > 0; // Return a constant expression
 }
 
 template<typename T, typename U>
 concept bool BinaryConcept() {
-	return sizeof(T) > 0 && sizeof(U) > 0;
+	return sizeof(T) > 0 && sizeof(U) > 0; // Of course logical operators are supported
 }
 
 template<typename T>
-concept bool AsVariableTemplateConcept = sizeof(T) > 0;
+concept bool AsVariableTemplateConcept = sizeof(T) > 0; 
 
 template<typename T>
-concept bool InvokingAnotherConcept = UnaryConcept<T>();
+concept bool InvokingAnotherConcept = UnaryConcept<T>(); // You can evaluate other concepts
 
 template<typename T>
 concept bool RequiresClause() {
 	return requires(T a, T b) {
-		{a == b},
-		{a == b} -> bool,
-		{a.f()},
-		{f(a)},
-		typename T::member_type,
-		template T::member_template,
-		{f(a)} -> UnaryConcept
+		a == b;           // a == b is valid syntax (i.e. exists operator==(T,T))
+		{a == b} -> bool; // a == b is valid syntax and returns bool
+		a.f();            // T has f() member function
+		f(a);             // there's a f(T) non-member function
+		typename T::member_type;     // T has "member_type" member type
+		template T::member_template; // T has "member_template" template
+		{f(a)} -> UnaryConcept // There's f(T) function and yields a type 
+		                       // that meets UnaryConcept concept
 	}
 }
 
@@ -47,6 +48,7 @@ struct PassesRequiresClauseExample {
 	template<typename...> struct member_template {};
 };
 
+// Note int meets UnaryConcept
 int f(const PassesRequiresClauseExample&) {
 	return 0;
 }
