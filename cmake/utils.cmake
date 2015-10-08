da1787558b20e1e88026f2683a8ada7f4b1941ed
Manu343726 <Manu343726@gmail.com>
2015-10-09 01:40:16 +0200

    Add utils.cmake to_list() changes
    Note travis build pases if cmake configure error.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -19,6 +19,12 @@ function(to_comma_list VAR)
 	set(${VAR} "${${VAR}}" PARENT_SCOPE)
 endfunction()
 
+function(to_list VAR DELIM)
+    string(REGEX REPLACE ";" "${DELIM}" ${VAR} "${${VAR}}")
+
+    set(${VAR} "${${VAR}}" PARENT_SCOPE)
+endfunction()
+
 function(to_fullpath LIST)
 	foreach(path ${${LIST}})
 		get_filename_component(fullpath ${path} ABSOLUTE)
